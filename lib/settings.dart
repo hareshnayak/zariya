4a49c5b36ff77a721e8168df30ab304079c8cb2d
Haresh Nayak <61956975+hareshnayak@users.noreply.github.com>
2021-01-06 14:12:52 +0530

    Create settings.dart
    empty at the time

---
+++ lib/settings.dart
@@ -0,0 +1,57 @@
+import 'package:flutter/material.dart';
+
+void main(){
+  runApp(MaterialApp(
+    debugShowCheckedModeBanner: false,
+    home: SettingsPage()));
+ 
+}
+
+class SettingsPage extends StatefulWidget {
+
+  @override
+  _SettingsPageState createState() => _SettingsPageState();
+}
+
+class _SettingsPageState extends State<SettingsPage> {  
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.white,
+        appBar: PreferredSize(
+          preferredSize: Size.fromHeight(45.0),
+          child: AppBar(
+            backgroundColor: Colors.transparent,
+            iconTheme: new IconThemeData(color: Colors.black),
+            title: Center(
+              child: Text(
+                'zariya',
+                style: TextStyle(
+                    fontWeight: FontWeight.bold,
+                    fontSize: 25,
+                    color: Colors.black),
+              ),
+            ),
+            elevation: 0,
+            actions: <Widget>[
+              SizedBox(
+                  width: 60,
+                  height: 30,
+                  child: FlatButton(
+                      padding: EdgeInsets.all(0),
+                      onPressed: () {},
+                      child: Icon(Icons.home, color: Colors.black)))
+            ],
+          ),
+        ),
+      body: Container(
+      child: ListView(
+      children: <Widget>[
+        Container(
+          padding: EdgeInsets.fromLTRB(20,10,20,20),
+          child: Text('Settings', style: TextStyle(fontSize: 30, fontWeight:FontWeight.bold, color: Colors.black),),),
+      ],),),
+    );
+  }
+}
